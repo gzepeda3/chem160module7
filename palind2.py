498b7a8a656692e8ff5e245ee3cb5c7987a7a909
gzepeda3 <66925740+gzepeda3@users.noreply.github.com>
2020-09-26 01:20:41 -0700

    Add files via upload

---
+++ palind2.py
@@ -0,0 +1,27 @@
+def palind(s):
+    rev=""
+    for i in s:
+        rev=i+rev
+    if rev==s:
+        return True
+    return False
+print(palind("madammimadam"))
+print(palind("madamimadam"))
+print(palind("ratsliveonnoevilstar"))
+print(palind("amanaplanacanalpanama"))
+
+def palind2(s):
+    alist=list(s)
+    alist.reverse()
+    revs="".join(alist)
+    if revs==s:
+        return True
+    return False
+print(palind2("ratsliveonnoevilstar"))
+print(palind2("abcdedcba"))
+
+#string.count(substring)
+#string.find(substring)
+#string.startswith(substring)
+#string.endswith("event")
+#Useful strings
